33e089da0ac93e3c942046d113e2db5ae22ae0ce
Bishwa Karki <karkeebishwa1@gmail.com>
2023-11-07 16:24:05 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv, find_dotenv
 
 def setup():
     sys.path.append('./content')
-    os.environ["OPENAI_API_KEY"] = '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'
+    os.environ["OPENAI_API_KEY"] = '#######'
     _ = load_dotenv(find_dotenv())
     openai.api_key  = os.environ['OPENAI_API_KEY']
